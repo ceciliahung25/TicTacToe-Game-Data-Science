593caba840643ba37f6cb960fff5d67d277e0895
ceciliahung25 <148398201+ceciliahung25@users.noreply.github.com>
2023-11-24 15:35:06 -0800

    Update cli.py

--- cli.py
+++ cli.py
@@ -2,7 +2,6 @@
 
 import csv
 import os
-from datetime import datetime
 from logic import Board, RandomBot
 
 def print_board(board):
@@ -14,57 +13,43 @@ def choose_player_type():
     while True:
         choice = input("Choose player type (1 for Human, 2 for RandomBot): ")
         if choice == '1':
-            return 'X', 'Human'
+            return 'X'
         elif choice == '2':
-            return 'O', 'RandomBot'
+            return 'O'
         else:
             print("Invalid choice. Please enter 1 or 2.")
 
 # 新增的函数，用于写入游戏数据到 CSV 文件
-def write_game_log(player_type, player_symbol, move_count, winner):
+def write_game_log(player, winner):
     log_file_path = 'logs/game_log.csv'
 
     # 如果文件不存在，创建文件并写入标题
     if not os.path.exists(log_file_path):
         with open(log_file_path, 'w', newline='') as csvfile:
-            fieldnames = ['Timestamp', 'Player Type', 'Player Symbol', 'Move Count', 'Winner']
+            fieldnames = ['Player', 'Winner']
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             writer.writeheader()
 
     # 将当前游戏数据写入文件
-    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     with open(log_file_path, 'a', newline='') as csvfile:
-        fieldnames = ['Timestamp', 'Player Type', 'Player Symbol', 'Move Count', 'Winner']
+        fieldnames = ['Player', 'Winner']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writerow({
-            'Timestamp': timestamp,
-            'Player Type': player_type,
-            'Player Symbol': player_symbol,
-            'Move Count': move_count,
-            'Winner': winner
-        })
+        writer.writerow({'Player': player, 'Winner': winner})
 
 def main():
     board = Board()
-    player_symbol, player_type = choose_player_type()
-    move_count = 0
+    player = choose_player_type()
 
     while True:
         print_board(board)
-        print(f"{player_type} Player {player_symbol}'s turn.")
+        print(f"Player {player}'s turn.")
 
-        if player_type == 'Human':
+        if player == 'X':
             row = int(input("Enter the row (0, 1, or 2): "))
             col = int(input("Enter the column (0, 1, or 2): "))
         else:
-            bot = RandomBot(player_symbol)
+            bot = RandomBot(player)
             move = bot.get_move(board)
-            
-            # 修改部分，确保 move 不为 None
-            while move is None:
-                print("RandomBot made an invalid move. Trying again.")
-                move = bot.get_move(board)
-
             row, col = move
 
         if 0 <= row < 3 and 0 <= col < 3:
@@ -72,21 +57,20 @@ def main():
                 print("That cell is already occupied! Try again.")
                 continue
 
-            board.grid[row][col] = player_symbol
-            move_count += 1
+            board.grid[row][col] = player
             winner = board.get_winner()
             if winner:
                 print_board(board)
-                print(f"{player_type} Player {player_symbol} wins!")
-                write_game_log(player_type, player_symbol, move_count, player_symbol)  # 将游戏数据写入 CSV 文件
+                print(f"Player {winner} wins!")
+                write_game_log(player, winner)  # 将游戏数据写入 CSV 文件
                 break
             elif all(cell is not None for row in board.grid for cell in row):
                 print_board(board)
                 print("It's a draw!")
-                write_game_log(player_type, player_symbol, move_count, "Draw")  # 将游戏数据写入 CSV 文件
+                write_game_log(player, "Draw")  # 将游戏数据写入 CSV 文件
                 break
             else:
-                player_symbol = board.other_player(player_symbol)
+                player = board.other_player(player)
         else:
             print("Invalid input! Row and column must be 0, 1, or 2.")
 
