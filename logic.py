b608a5caed7bc28046dcf6225ec1acebb5938f61
ceciliahung25 <148398201+ceciliahung25@users.noreply.github.com>
2023-11-24 16:38:01 -0800

    Update logic.py

--- logic.py
+++ logic.py
@@ -16,58 +16,32 @@ class Board:
         ]
 
     def other_player(self, player):
-        return 'O' if player == 'X' else 'X'
-
-    def make_move(self, player, row, col):
-        if self.grid[row][col] is not None:
-            raise ValueError("Selected spot is already occupied")
-        self.grid[row][col] = player
+        if player == 'X':
+            return 'O'
+        else:
+            return 'X'
 
     def get_winner(self):
         # Check rows
         for row in self.grid:
-            if row.count('X') == 3:
-                print("Row Winner: X")
-                return 'X'
-            elif row.count('O') == 3:
-                print("Row Winner: O")
-                return 'O'
-    
+            if row[0] == row[1] == row[2] and row[0] is not None:
+                return row[0]
+
         # Check columns
         for col in range(3):
-            column = [self.grid[row][col] for row in range(3)]
-            if column.count('X') == 3:
-                print("Column Winner: X")
-                return 'X'
-            elif column.count('O') == 3:
-                print("Column Winner: O")
-                return 'O'
-    
+            if self.grid[0][col] == self.grid[1][col] == self.grid[2][col] and self.grid[0][col] is not None:
+                return self.grid[0][col]
+
         # Check diagonals
-        diagonal1 = [self.grid[i][i] for i in range(3)]
-        diagonal2 = [self.grid[i][2 - i] for i in range(3)]
-    
-        if diagonal1.count('X') == 3:
-            print("Diagonal Winner: X")
-            return 'X'
-        elif diagonal1.count('O') == 3:
-            print("Diagonal Winner: O")
-            return 'O'
-        
-        if diagonal2.count('X') == 3:
-            print("Diagonal Winner: X")
-            return 'X'
-        elif diagonal2.count('O') == 3:
-            print("Diagonal Winner: O")
-            return 'O'
-    
-        # Print the board state before returning None
-        print("No winner yet. Current board state:")
-        for row in self.grid:
-            print(row)
-    
+        if self.grid[0][0] == self.grid[1][1] == self.grid[2][2] and self.grid[0][0] is not None:
+            return self.grid[0][0]
+        if self.grid[0][2] == self.grid[1][1] == self.grid[2][0] and self.grid[0][2] is not None:
+            return self.grid[0][2]
+
         return None
 
+    def get_empty_squares(self):
+        return [(i, j) for i in range(3) for j in range(3) if self.grid[i][j] is None]
 
 class RandomBot:
     def __init__(self, symbol):
@@ -76,14 +50,3 @@ class RandomBot:
     def get_move(self, board):
         available_squares = board.get_empty_squares()
         return random.choice(available_squares) if available_squares else None
-
-
-def choose_player_type():
-    while True:
-        choice = input("Choose player type (1 for Human, 2 for RandomBot): ")
-        if choice == '1':
-            return 'X'
-        elif choice == '2':
-            return 'O'
-        else:
-            print("Invalid choice. Please enter 1 or 2.")
