4628a39e77309299fbd0a5d0a05043a5fc7704ef
ceciliahung25 <148398201+ceciliahung25@users.noreply.github.com>
2023-11-24 19:38:24 -0800

    Update cli.py

--- cli.py
+++ cli.py
@@ -35,4 +35,51 @@ def play_game(board):
             row, col = move
 
         if 0 <= row < 3 and 0 <= col < 3:
-            if board.gri
+            if board.grid[row][col] is not None:
+                print("That cell is already occupied! Try again.")
+                continue
+
+            board.grid[row][col] = player
+            board.record_move(player, row, col)  # 记录游戏数据
+            winner = board.get_winner()
+            if winner:
+                print_board(board)
+                print(f"Player {winner} wins!")
+                break
+            elif all(cell is not None for row in board.grid for cell in row):
+                print_board(board)
+                print("It's a draw!")
+                break
+            else:
+                player = board.other_player(player)
+        else:
+            print("Invalid input! Row and column must be 0, 1, or 2.")
+
+    # 游戏结束后写入CSV文件
+    write_csv(board)
+
+def write_csv(board):
+    file_path = os.path.join('logs', 'game_log.csv')
+    fieldnames = ['player', 'row', 'col', 'board', 'result']
+
+    # 检查文件是否存在，如果不存在则写入表头
+    if not os.path.exists(file_path):
+        with open(file_path, mode='w', newline='') as csv_file:
+            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+            writer.writeheader()
+
+    # 写入所有游戏日志数据
+    with open(file_path, mode='a', newline='') as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        writer.writerows(board.game_log)
+
+def main():
+    while True:
+        board = Board()
+        play_game(board)
+        restart = input("Do you want to play again? (yes/no): ")
+        if restart.lower() != 'yes':
+            break
+
+if __name__ == '__main__':
+    main()
